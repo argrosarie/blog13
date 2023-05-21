0a9613aad4791013fd9f8c32b25fd2f1cba0d1f3
rosarie <rosariosanchezsampietro@gmail.com>
2023-05-21 16:31:33 -0300

    Folder setup and first header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Title h1</h1>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">
+        Lorem ipsum dolor sit amet.
+      </span>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </section>
+  );
 };
 
 export default Home;
